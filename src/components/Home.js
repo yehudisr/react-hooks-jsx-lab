dd87ab1c2d72b880acbf3b44d904fd3247644bd4
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-05 14:25:22 -0500

    commit tree 80ba6afa0eabc5a060dc652d3863fbda11ed8597

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+  <h1 style = {{color:"firebrick"}}> {name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
+
+
+// a <div> element with an id of "home"
+// inside the div element, there should be one child element:
+// a <h1> element, with the style attribute set to a color of "firebrick", and the content of "Your Name is a Web Developer from Your City"
+// use the imported name and city variables within the component, and the JSX expression syntax to use the variables inside your JSX elements
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>;
 }
 
 export default NavBar;
+
+
+// an <a> element, with the href attribute set to #home, and the content of "Home"
+// an <a> element, with the href attribute set to #about, and the content of "About"
